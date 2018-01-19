e95219964a66ddea09ac3a505f2580e255b092f6
nathakits <nathakit.sae-tan@cuinda.com>
2018-01-19 18:17:00 +0700

    add gulp and node packages

---
+++ my-app/gulpfile.js
@@ -0,0 +1,78 @@
+// load all required libraries
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const log = require('fancy-log');
+
+const File = gutil.File;
+
+const markdownToJSON = require('gulp-markdown-to-json');
+const marked = require('marked');
+const flatten = require('gulp-flatten');
+const concat_json = require("gulp-combine-json");
+const runSeq = require('run-sequence');
+
+
+// =================================================================
+// default tasks
+// =================================================================
+
+gulp.task('default', function() {
+  return runSeq(
+    'md-posts',
+    'md-concat'
+    );
+});
+
+// =================================================================
+// mardown tasks
+// =================================================================
+
+
+var md_path = './src/articles/**/*.md';
+var json_path = './src/articles/**/*.json';
+
+marked.setOptions({
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
+
+// generate json from all md's into one single file
+gulp.task('mark-all', () => {
+  gulp.src(md_path)
+    .pipe(gutil.buffer())
+    .pipe(markdownToJSON(marked, (data) => {
+      delete data.body;
+      return data
+    }))
+    .pipe(flatten())
+    .pipe(gulp.dest('./src/json/'))
+});
+
+// generates json files for each md
+gulp.task('md-posts', () => {
+  gulp.src(md_path)
+    .pipe(markdownToJSON(marked, (data, file) => {
+      // delete data.body;
+      
+      // set folder name
+      var path = file.path
+      var folder_name = path.match(/\d..([(0-9])(-)\w.+(?=\/)/g);
+      data.article = folder_name.toString();
+      var array = [];
+      array.push(data)
+      return array
+    }))
+    .pipe(gulp.dest('./src/articles/'))
+});
+
+// combine all generated json files into one single file
+gulp.task('md-concat', () => {
+  gulp.src(json_path)
+    .pipe(concat_json("combined.json"))
+    .pipe(gulp.dest('./src/json'));
+});
